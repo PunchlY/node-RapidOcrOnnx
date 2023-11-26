719ca8135b77311fbcb5f1125ad9c3fc7f0c805a
PunchlY <punchly@foxmail.com>
2023-11-27 02:23:30 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,18 @@
 import OCR from './RapidOcrOnnx.cjs';
+import fs from 'fs/promises';
 
 setInterval(Function.prototype, -1 >>> 1);
 
-const ocr = new OCR();
-console.log('---')
+const ocr = new OCR({
+    detModel: 'models/ch_PP-OCRv3_det_infer.onnx',
+    clsModel: 'models/ch_ppocr_mobile_v2.0_cls_infer.onnx',
+    recModel: 'models/ch_PP-OCRv3_rec_infer.onnx',
+    keyPath: 'models/ppocr_keys_v1.txt',
+});
+console.log('---');
+
+// console.log(ocr.detectSync('images/1.jpg'));
+
 ocr.detect('images/1.jpg').then(console.log).catch(console.error);
+
+// ocr.detect(await fs.readFile('images/1.jpg')).then(console.log).catch(console.error);
