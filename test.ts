6a9fd1e6c75b334dffa705e08108259ee0e138e0
PunchlY <punchly@foxmail.com>
2023-11-26 23:11:00 +0800

    Changes to be committed:

---
+++ test.ts
@@ -0,0 +1,7 @@
+import OCR from './RapidOcrOnnx.cjs';
+
+setInterval(Function.prototype, -1 >>> 1);
+
+const ocr = new OCR();
+console.log('---')
+ocr.detect('images/1.jpg').then(console.log).catch(console.error);
